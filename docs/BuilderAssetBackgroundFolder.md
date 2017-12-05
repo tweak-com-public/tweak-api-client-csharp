54d552ac4540c2f1b979e20fc9c3aeb0f676a7b7
Matteo Zambon <matteo@harpoonconnect.com>
2017-12-05 13:56:57 +0100

    Added Builder Asset Backgrounds (private/public)

---
+++ docs/BuilderAssetBackgroundFolder.md
@@ -0,0 +1,11 @@
+# TweakApi.Model.BuilderAssetBackgroundFolder
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**Name** | **string** |  | 
+**Path** | **string** |  | 
+**Id** | **string** |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
--- src/TweakApi/Client/Configuration.cs
+++ src/TweakApi/Client/Configuration.cs
@@ -58,7 +58,7 @@ namespace TweakApi.Client
                              string tempFolderPath = null,
                              string dateTimeFormat = null,
                              int timeout = 100000,
-                             string userAgent = "Swagger-Codegen/1.0.3-alpha.8/csharp"
+                             string userAgent = "Swagger-Codegen/1.0.3-alpha.9/csharp"
                             )
         {
             setApiClientUsingDefault(apiClient);
@@ -93,7 +93,7 @@ namespace TweakApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.3-alpha.8";
+        public const string Version = "1.0.3-alpha.9";
 
         /// <summary>
         /// Gets or sets the default Configuration.
@@ -330,7 +330,7 @@ namespace TweakApi.Client
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
             report += "    Version of the API: 1.0.3-alpha.11\n";
-            report += "    SDK Package Version: 1.0.3-alpha.8\n";
+            report += "    SDK Package Version: 1.0.3-alpha.9\n";
 
             return report;
         }
